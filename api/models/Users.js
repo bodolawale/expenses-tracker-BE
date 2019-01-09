d9cbd59ad9449332597ba1149249b1a87788d138
bodolawale <bodolawale@gmail.com>
2019-01-09 14:47:47 +0100

    initial commit

---
+++ api/models/Users.js
@@ -0,0 +1,32 @@
+/**
+ * Users.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    firstName: {
+      type: 'string',
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      required: true
+    },
+    email : {
+      type: 'string',
+      isEmail: true,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    }
+  },
+  datastore: 'mongodb'
+};
+
