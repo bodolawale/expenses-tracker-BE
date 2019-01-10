55968315dfcdbccfd39e1c4bcca1799d7d548b05
bodolawale <bodolawale@gmail.com>
2019-01-10 17:47:50 +0100

    Finished user CRUD and added jwt  authentication

--- api/models/User.js
+++ api/models/User.js
@@ -34,6 +34,26 @@ module.exports = {
     // Return a shallow copy of this record with the password and ssn removed.
     return _.omit(this, ['password', 'ssn']);
   },
+  beforeUpdate: function (user, cb) {
+    if (user.password) {
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          return cb(err);
+        } else {
+          bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+              return cb(err);
+            } else {
+              user.password = hash;
+              return cb(null,user);
+            }
+          });
+        }
+      });
+    } else {
+      return cb();
+    }
+  },
   beforeCreate: function (user, cb) {
     bcrypt.genSalt(10, (err, salt) => {
       if (err) {
